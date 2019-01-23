4b4b9eff9971ed7fbd4ea18e404d80733b135564
jrntky <jnrkty1@naver.com>
2019-01-23 16:38:08 +0900

    게시판 리스트 페이지 구현

--- spring-test/src/main/java/kr/green/springtest/service/BoardServiceImp.java
+++ spring-test/src/main/java/kr/green/springtest/service/BoardServiceImp.java
@@ -1,5 +1,21 @@
 package kr.green.springtest.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.green.springtest.dao.BoardDao;
+import kr.green.springtest.vo.BoardVo;
+
+@Service
 public class BoardServiceImp implements BoardService{
+	@Autowired
+	BoardDao boardDao;
+
+	@Override
+	public List<BoardVo> getBoards() {
+		return boardDao.getBoards();
+	}
 
 }
