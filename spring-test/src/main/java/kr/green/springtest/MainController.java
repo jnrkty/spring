7b302d1dce9f447354d5ddf9d92a30f1a88f971d
jrntky <jnrkty1@naver.com>
2019-01-23 10:08:10 +0900

    컨트롤러 패키지 수정 및 파일명 수정및 로그인정보 출력 테스트

--- spring-test/src/main/java/kr/green/springtest/MainController.java
+++ spring-test/src/main/java/kr/green/springtest/MainController.java
@@ -15,25 +15,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class MainController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String homeGet(Model model) {
 		return "home";
+	}
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String homePost(Model model, String id, String pw) {
+		System.out.println("id : " +id+", pw : "+pw);
+		return "redirect:/";
 	}
 	
 }
