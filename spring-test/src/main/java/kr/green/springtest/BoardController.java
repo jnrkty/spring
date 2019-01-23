997e468196ecc7fb3a94ea857036e3645c5197d3
jrntky <jnrkty1@naver.com>
2019-01-23 14:20:04 +0900

    BoardController생성 및 bbs/list.jsp추가

---
+++ spring-test/src/main/java/kr/green/springtest/BoardController.java
@@ -0,0 +1,13 @@
+package kr.green.springtest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+	@RequestMapping(value="/bbs/list")
+	public String list() {
+		return "bbs/list";
+	}
+
+}
